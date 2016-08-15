b85fb99269cb01b81ad98ab42a700c5627043fcc
Aditya <nagamalliaditya3@gmail.com>
2016-08-15 23:12:24 +0530

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,sum=0,i;
+clrscr();
+printf("enter the range for n");
+scanf("%d", &n);
+
+for(i=0;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("the sum of natural numbers is %d", sum);
+getch();
+return 0;
+}
